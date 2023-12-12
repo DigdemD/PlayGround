ddd0d8b00b3735c22409cd3b2306a0ae0e5fc3c8
Nur Digdem Deyirmenci <150827069+DigdemD@users.noreply.github.com>
2023-12-12 20:46:29 +0000

    to be able to understand and make practice on account class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("Hi coyote, let's have some JavaScript fun!")
+class bankAccount {
+  #balance = 0
+  #transactions = []
+  #overdraft = 100
+
+  #setBalance = newAmount => {
+    if (newAmount < -this.#overdraft) throw new Error(`Balance can not go below -${this.#overdraft}`)
+
+    this.#transactions.push(newAmount)
+    this.#balance = newAmount
+  }
+
+  get balance() {
+    return this.#balance
+  }
+
+  deposit(amount) {
+    if (amount <= 0) throw new Error(`Amount must be positive number`)
+    this.#setbalance(this.#balance + amount)
+  }
+
+  withdraw(amount) {
+    if (amount <= 0) throw new Error(`Amount must be positive number`)
+    this.#setbalance(this.#balance - amount)
+  }
+
+  get transactions() {
+    return this.#transactions.slice()
+  }
+}
+
+const account = new bankAccount()
+account.withdraw(-100)
+
+console.log('account balance', account.balance)
+console.log('transaction history', account.transactions)
